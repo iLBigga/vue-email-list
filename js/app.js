06ecfdd438c343a17681a8cf8a3496b6c4bb59b5
iLBigga <Giannanti1996@gmail.com>
2022-09-01 15:24:59 +0200

    Creata lista di email tramite API

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const app = new Vue({
+    el: '#app',
+    data:{
+        randomEmail: [],
+        email: null,
+    },
+    mounted() {
+        for(i = 0; i < 10; i++){
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((res) => {
+                this.email = res.data.response;
+                this.randomEmail.push(this.email)
+                });
+        };
+    },
+});
