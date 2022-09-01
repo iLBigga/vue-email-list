155eafb2219b814497c3d05f79b0a3b00bbcf500
iLBigga <Giannanti1996@gmail.com>
2022-09-01 16:10:40 +0200

    Refactory

--- js/app.js
+++ js/app.js
@@ -2,14 +2,12 @@ const app = new Vue({
     el: '#app',
     data:{
         randomEmail: [],
-        email: null,
     },
     mounted() {
         for(i = 0; i < 10; i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then((res) => {
-                    this.email = res.data.response;
-                    this.randomEmail.push(this.email)
+                    this.randomEmail.push(res.data.response)
                 });
         };
     },
